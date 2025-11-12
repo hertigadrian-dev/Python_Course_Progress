eaf0edf5542d381b415679c4c9e3eee470896c52
hertigadrian-dev <hertig.adrian.dev@gmail.com>
2025-11-12 16:30:29 +0100

    Update equation_grad1_passwd.py

--- 07_Mini_Projects/equation_grad1_passwd.py
+++ 07_Mini_Projects/equation_grad1_passwd.py
@@ -14,4 +14,55 @@ def password():
 	else:
 		print('your tries are up!')
 		return False
-password()
+
+def access_level():
+	level = password()
+	if level:
+		print('Welcome to level 2 !')
+		print('--------------------------------')
+		main()
+
+	else:
+		print('\nAccess denied, try later!')
+
+''' We solve equation ax + b = c '''
+
+def solve_equation(a,b,c):
+	if a == 0:
+		return None
+	return (c-b)/a
+
+def format_number(n):
+	return str(int(n)) if n.is_integer() else f'{n:.2f}'
+
+def main():
+	print('Solve equation ax + b = c')
+	print('-----------------------------------')
+
+	while True:
+		try:
+			a = float(input('insert the value of a:'))
+			b = float(input('insert the value of b:'))
+			c = float(input('insert the value of c:'))
+
+			x = solve_equation(a,b,c)
+
+			if x == None:
+				print('a can\'t be zero !')
+				continue
+
+			sign = '+' if b > 0 else '-'
+
+			print(f'your equation is: {format_number(a)}x{sign}{format_number(abs(b))} = {format_number(c)}')
+
+			if x.is_integer():
+				print(f'x={int(x)}')
+			else:
+				print(f'x={x:.2f}')
+		except ValueError:
+			print('Insert only numbers not letters !')
+if __name__ == '__main__':
+	access_level()
+
+
+
