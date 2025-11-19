dad1b72e5d11ba5b115cd8079a5916091fd08e6f
hertigadrian-dev <hertig.adrian.dev@gmail.com>
2025-11-19 23:27:37 +0100

    playing around

--- 08_Using_Python_to_Access_Web_Data/sockets_in_Python.py
+++ 08_Using_Python_to_Access_Web_Data/sockets_in_Python.py
@@ -16,51 +16,58 @@
 
 
 
+# import socket 
+
+# canal = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# canal.connect(('data.pr4e.org', 80))
+
+
+# cmd = (
+# 	'GET /romeo.txt HTTP/1.1\r\n'
+# 	'Host: data.pr4e.org\r\n' 
+# 	'Connection: close\r\n'
+# 	'\r\n'
+
+# ).encode()
+
+# canal.send(cmd)
+
+# while True:
+# 	data = canal.recv(1000)
+# 	if len(data) < 1:
+# 		break
+# 	print(data.decode())
+
+# canal.close()
+
+# #---------------------------
+
 import socket 
 
-canal = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-canal.connect(('data.pr4e.org', 80))
+phone = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+result = phone.connect_ex(('data.pr4e.org', 80))
+if result != 0:
+	print('connection error, error code:', result)
+	exit()
 
-
-cmd = (
-	'GET /romeo.txt HTTP/1.1\r\n'
-	'Host: data.pr4e.org\r\n' 
-	'Connection: close\r\n'
-	'\r\n'
-
-).encode()
-
-canal.send(cmd)
-
-while True:
-	data = canal.recv(1000)
-	if len(data) < 1:
-		break
-	print(data.decode())
-
-canal.close()
-
-#---------------------------
-
-import socket 
-
-canal = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # I am creating a network connection pipe using IPv4 and TCP
-canal.connect(('data.pr4e.org', 80)) # opens a TCP connection to Hostname: data.pr4e.org and Port: 80
-
-cmd = (
-	'GET /romeo.txt HTTP/1.1\r\n'
+my_request = (
+	'GET /page1.html HTTP/1.1\r\n'
 	'Host: data.pr4e.org\r\n'
 	'Connection: close\r\n'
 	'\r\n'
 	).encode()
+phone.send(my_request)
 
-canal.send(cmd)
+while True:
+	data = phone.recv(500)
+	if len(data) < 1:
+		break
+	print(data.decode())
+phone.close()
 
 
 
-
-
-
+#-------------------------------------------
 
 
 
