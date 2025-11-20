d6f6d00f2c3395dc479e622295bd04c5f021f772
hertigadrian-dev <hertig.adrian.dev@gmail.com>
2025-11-20 20:39:36 +0100

    Update requests.py

--- 08_Using_Python_to_Access_Web_Data/requests.py
+++ 08_Using_Python_to_Access_Web_Data/requests.py
@@ -1,14 +1,12 @@
 
-
-
-
 import requests
+import socket
 
 url = "https://api.coingecko.com/api/v3/simple/price"
 params = {
     "ids": "bitcoin",
     "vs_currencies": "usd,eur,gbp"
-}
+    }
 
 response = requests.get(url, params=params)
 data = response.json()
