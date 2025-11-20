efb7dd1bf8bd87df05fc589991f4f9a85dcf103d
hertigadrian-dev <hertig.adrian.dev@gmail.com>
2025-11-20 22:20:17 +0100

    using urllib NO Headers at print

--- 08_Using_Python_to_Access_Web_Data/sockets_in_Python.py
+++ 08_Using_Python_to_Access_Web_Data/sockets_in_Python.py
@@ -1,89 +1,38 @@
-# import socket #This module allows your program to communicate over the internet (TCP, UDP, etc.).
+# import socket
 
-# mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # AF_INET = IPv4 / SOCK_STREAM = TCP / socket.socket(...) - Creates a “network connection object” 
+# phone = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# data = phone.connect_ex(('data.pr4e.org', 80))
 
-# mysock.connect(('data.pr4e.org', 80))
+# if data != 0:
+# 	print('connection failed! error code:', data)
+# 	exit()
 
-# cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
-# mysock.send(cmd)
-
-# while True:
-# 	data = mysock.recv(512)
-# 	if (len(data) < 1):
-# 		break
-# 	print (data.decode())
-# mysock.close()
-
-
-
-# import socket 
-
-# canal = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# canal.connect(('data.pr4e.org', 80))
-
-
-# cmd = (
+# my_request = (
 # 	'GET /romeo.txt HTTP/1.1\r\n'
-# 	'Host: data.pr4e.org\r\n' 
+# 	'Host: data.pr4e.org\r\n'
 # 	'Connection: close\r\n'
 # 	'\r\n'
+# 	).encode()
 
-# ).encode()
-
-# canal.send(cmd)
+# phone.send(my_request)
 
 # while True:
-# 	data = canal.recv(1000)
-# 	if len(data) < 1:
+# 	ceva = phone.recv(500)
+# 	if len(ceva) < 1:
 # 		break
-# 	print(data.decode())
-
-# canal.close()
-
-# #---------------------------
-
-import socket
-
-phone = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-result = phone.connect_ex(('data.pr4e.org', 80))
-
-if result != 0:
-	print('connection failed, error code:', result)
-	exit()
-
-my_request = (
-	'GET /romeo.txt HTTP/1.1\r\n'
-	'Host: data.pr4e.org\r\n'
-	'Connection: close\r\n'
-	'\r\n'
-	).encode()
-phone.send(my_request)
+# 	print(phone.decode())
+# phone.close()
 
 
-while True:
-	data = phone.recv(500)
-	if len(data) < 1:
-		break
-	print(data.decode())
-phone.close()
+#------------------------
+''' Shorter version '''
+'''   No Headers   ''' 
+#------------------------
 
+import socket 
+import urllib.request, urllib.parse, urllib.error
 
-
-
-
-
-
-
-
-
-
-
-
-#-------------------------------------------
-
-
-
-
-
-
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+for line in fhand:
+	print(line.decode().strip())
 
