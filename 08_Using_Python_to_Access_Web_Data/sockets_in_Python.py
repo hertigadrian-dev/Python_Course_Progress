ddd14b013b357c99ab82a89114855c0b9492f7c3
hertigadrian-dev <hertig.adrian.dev@gmail.com>
2025-11-22 21:11:35 +0100

    Update sockets_in_Python.py

--- 08_Using_Python_to_Access_Web_Data/sockets_in_Python.py
+++ 08_Using_Python_to_Access_Web_Data/sockets_in_Python.py
@@ -1,52 +1,65 @@
-# import socket
+#---------------------
+# using socket
+#---------------------
 
-# phone = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# data = phone.connect_ex(('data.pr4e.org', 80))
 
-# if data != 0:
-# 	print('connection failed! error code:', data)
-# 	exit()
+import socket
 
-# my_request = (
-# 	'GET /romeo.txt HTTP/1.1\r\n'
-# 	'Host: data.pr4e.org\r\n'
-# 	'Connection: close\r\n'
-# 	'\r\n'
-# 	).encode()
+phone = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+data = phone.connect_ex(('data.pr4e.org', 80))
 
-# phone.send(my_request)
+if data != 0:
+	print('connection failed! error code:', data)
+	exit()
 
-# while True:
-# 	ceva = phone.recv(500)
-# 	if len(ceva) < 1:
-# 		break
-# 	print(phone.decode())
-# phone.close()
+my_request = (
+	'GET /romeo.txt HTTP/1.1\r\n'
+	'Host: data.pr4e.org\r\n'
+	'Connection: close\r\n'
+	'\r\n'
+	).encode()
+
+phone.send(my_request)
+
+while True:
+	ceva = phone.recv(500)
+	if len(ceva) < 1:
+		break
+	print(phone.decode())
+phone.close()
 
 
 #------------------------
-''' Shorter version '''
-'''   No Headers   ''' 
-#------------------------
-#ex.1
-# import socket 
-# import urllib.request, urllib.parse, urllib.error
 
-# fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
-# for line in fhand:
-# 	print(line.decode().strip())
 
-#ex.2
-import socket 
-import urllib.request, urllib.parse, urllib.error
 
-fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
-adri = {}
-for line in fhand:
-	words = line.decode().split()
-	for stuff in words:
-		adri[stuff] = adri.get(stuff, 0) + 1
-print(max(adri))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
