cb0fde128d333692c76cb255c117153d451bb8cd
hertigadrian-dev <hertig.adrian.dev@gmail.com>
2025-11-23 00:27:35 +0100

    Update Finding_Numbers_in_a_Haystack.py

--- 08_Using_Python_to_Access_Web_Data/Finding_Numbers_in_a_Haystack.py
+++ 08_Using_Python_to_Access_Web_Data/Finding_Numbers_in_a_Haystack.py
@@ -30,9 +30,7 @@ The sum for the sample text above is 27486. The numbers can appear anywhere in t
 Handling The Data
 The basic outline of this problem is to read the file, look for integers using the re.findall(), looking for a regular expression of '[0-9]+' and then converting the extracted strings to integers and summing up the integers.
 
-  '''
-
-
+  '''	
 
 #---------------------------------------
 #For file: 'regex_sum_42.txt' we have:
