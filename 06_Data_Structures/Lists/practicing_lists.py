fc027b6187f1b07a9ac0f84d9adf78c6e690b6af
hertigadrian-dev <hertig.adrian.dev@gmail.com>
2025-11-30 00:17:48 +0100

    eliminate whitespace from a list

--- 06_Data_Structures/Lists/practicing_lists.py
+++ 06_Data_Structures/Lists/practicing_lists.py
@@ -1,3 +1,26 @@
 x =list()
 x+=['para','mar'] # the same as: x.extend(['para', 'mar']) 
 print(x)
+
+#------------------------------------
+#ex: eliminate whitespace/(index[1]):
+#------------------------------------
+
+word2 = ['https:', '', 'www.youtube.com', 'watch?v=BVKpW02hsrU']
+
+
+adri_list = []
+
+for x in word2:
+	if x:
+		adri_list.append(x)
+word2 = adri_list
+print(word2)
+
+#-------------------------------------------
+''' the same thing as above in one line: '''
+#-------------------------------------------
+
+word2 = [x for x in word2 if x]
+print(word2)
+
